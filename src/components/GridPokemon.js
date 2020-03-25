9de52ac922e4960be1438b38257223f1c67934dd
Raphael2790 <rssshev@gmail.com>
2020-03-25 15:57:53 -0300

    Trying to render pokemons in pokedex page

--- src/components/GridPokemon.js
+++ src/components/GridPokemon.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
 
 const {bancoPokemon} = require('../data')
 
@@ -8,9 +9,27 @@ export default class GridPokemon extends Component {
   
   render() {
     return (
-      <div>
-        {this.state.Name}
-      </div>
+      <Container>
+        {this.state.map((item)=>{
+
+          return  <Container>
+                  <Row>
+                  <Col sm={4}><p>{item.Name}</p></Col>
+                  <Col sm={4}><p>{item.Row}</p></Col>
+                  <Col sm={4}><p>{item.Generation}</p></Col>
+                  <Col sm={4}><p>{item.Name}</p></Col>
+                  <Col sm={4}><p>{item.Row}</p></Col>
+                  <Col sm={4}><p>{item.Generation}</p></Col>
+                  <Col sm={4}><p>{item.Name}</p></Col>
+                  <Col sm={4}><p>{item.Pokedex}</p></Col>
+                  <Col sm={4}><p>{item.Generation}</p></Col>
+                  <Col sm={4}><p>{item.Name}</p></Col>
+                  <Col sm={4}><p>{item.Pokedex}</p></Col>
+                  <Col sm={4}><p>{item.Generation}</p></Col>
+                  </Row>
+                  </Container>
+        })}
+      </Container>
     )
   }
 }
